8853ea8175af0b10e8d033bde992ed131f0f5cc9
maxime-pierrard <maxime.pierrard@edu.esiee.fr>
2024-11-07 10:17:11 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+"""
+Bonjour
+"""
 from math import sqrt
-
-#### Fonction secondaire
-
-
 def isprime(p):
+    """
+    Voir si c'est premier
+    """
+    if p <= 1:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
-    # votre code ici
-
-    pass
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    module main
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
